963788ac22b5e302c49ed01a24b488afaca5485b
David Brady <dgrbrady@gmail.com>
2022-03-02 11:37:28 -0500

    installing eslint and prettier and fixing lint issues

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { ScullyRoutesService } from '@scullyio/ng-lib';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   links$ = this.scully.available$;
